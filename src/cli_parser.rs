e7ba0e2649f663a411c61da0e1e43574e849c6c9
suquiya <22910980+suquiya@users.noreply.github.com>
2019-12-01 15:22:38 +0900

    コマンドラインからの実行部分を作成開始

--- src/cli_parser.rs
+++ src/cli_parser.rs
@@ -1,24 +1,26 @@
 use clap::{Arg, SubCommand};
 
-pub fn default<'a, 'b>() -> clap::App<'a, 'b> {
+pub fn nibi_basic<'a, 'b>() -> clap::App<'a, 'b> {
+    let arg_config = Arg::with_name("config")
+        .help("config file if want to specify")
+        .short("c")
+        .long("config")
+        .takes_value(true);
     return app_from_crate!()
         .arg(
             Arg::with_name("buildTarget")
             .help("Target directories for compile with nibi. This argment is not given, target directory will be current directory.")
             .default_value(".")
         ).arg(
-            Arg::with_name("config")
-                .help("config file if want to specify")
-                .short("c")
-                .long("config")
-                .takes_value(true),
+            arg_config.clone()
         )
         .subcommand(
             SubCommand::with_name("new")
-                .about("create new nibi object.Out of nibi project, create project named <objectName>. In nibi project, create post named <objectname>.")
-                .arg(Arg::with_name("objectName").help("object name"))
                 .subcommand(SubCommand::with_name("post"))
-                .subcommand(SubCommand::with_name("proj"))
+                .subcommand(SubCommand::with_name("proj")
+                    .about("create new nibi object. Out of nibi project, create project named <projectName>.")
+                    .arg(Arg::with_name("projectName").help("project name"))
+                )
                 .subcommand(SubCommand::with_name("theme")),
         )
         .subcommand(
@@ -33,6 +35,7 @@ pub fn default<'a, 'b>() -> clap::App<'a, 'b> {
             SubCommand::with_name("init")
                 .about("init target directory for nibi")
                 .arg(Arg::with_name("initTarget").help("directory name for new project.").default_value("."))
+                .arg(arg_config)
         )
         .subcommand(
             SubCommand::with_name("list")
--- src/cmd.rs
+++ src/cmd.rs
@@ -12,8 +12,29 @@ pub fn exec_from_cli<'a, 'b>(cli: clap::App<'a, 'b>) {
     }
 
     match matches.subcommand() {
-        ("init", Some(_)) => {}
-        ("new", Some(_)) => {}
+        ("init", Some(sub_matches)) => {
+            if let Some(init_target_dir) = sub_matches.value_of("initTarget") {
+                init(init_target_dir);
+            }
+        }
+        ("new", Some(sub_matches)) => match sub_matches.subcommand() {
+            ("post", Some(_)) => {}
+            ("proj", Some(args_matches)) => {
+                if let Some(project_name) = args_matches.value_of("project_name") {
+                    new_proj(project_name)
+                }
+            }
+            ("theme", Some(_)) => {}
+            (_, _) => {}
+        },
         (_, _) => {}
     }
+}
+
+fn init(target: &str) {
+    println!("{}", target);
+}
+
+fn new_proj(project_name: &str) {
+    println!("{}", project_name);
 }
