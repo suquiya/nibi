f2731c105800bef0cbdf6562da88943fcfca9ec9
suquiya <22910980+suquiya@users.noreply.github.com>
2025-08-17 23:51:00 +0900

    feat: changing cliclack to demand, but wait deps update

--- src/cli/prompt.rs
+++ src/cli/prompt.rs
@@ -1,7 +1,7 @@
-use cliclack::{confirm, input, select};
+use demand::{Confirm, DemandOption, Input, Select, Spinner};
 
 pub fn yes_or_no(message: &str) -> Option<bool> {
-	confirm(message).interact().ok()
+	Confirm::new(message).run().ok()
 }
 
 pub fn yes_or_no_with_default(message: &str, default: bool) -> bool {
@@ -9,23 +9,43 @@ pub fn yes_or_no_with_default(message: &str, default: bool) -> bool {
 }
 
 pub fn inquiry_str(message: &str, default: &str) -> String {
-	input(message)
-		.default_input(default)
-		.interact()
+	let title = message.to_owned() + ": ";
+	Input::new(title)
+		.inline(true)
+		.placeholder(default)
+		.default_value(default)
+		.validation(|s| {
+			if s.is_empty() {
+				Ok(())
+			} else {
+				Err("空文字は入力できません")
+			}
+		})
+		.run()
 		.ok()
 		.unwrap_or(default.into())
 }
 
 pub fn selector(message: &str, options: &[&str], default: &str) -> String {
-	let mut s = select(message);
+	let mut s = Select::new(message);
 	for option in options {
-		let opt_val = *option;
-		s = s.item(
-			opt_val,
-			opt_val,
-			if opt_val == default { "default" } else { "" },
-		);
+		let demand_option = if *option == default {
+			DemandOption::new(*option).selected(true)
+		} else {
+			DemandOption::new(*option)
+		};
+		s = s.option(demand_option);
 	}
 
-	s.interact().ok().unwrap_or(default).into()
+	s.run().ok().unwrap_or(default).into()
+}
+
+pub fn run_with_spinner<'scope, 'spinner: 'scope, F>(
+	message: &str,
+	func: F,
+) -> Result<String, String>
+where
+	F: FnOnce(&mut SpinnerActionRunner<'spinner>) -> String + Send + 'scope,
+{
+	let r = Spinner::new(message).run(func);
 }
